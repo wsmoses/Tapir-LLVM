dc7cd94ca46ba477e113d2844de893b82b95b081
TB Schardl <neboat@mit.edu>
2015-09-20 19:05:06 +0000

    Updated AliasAnalysis to analyze detached blocks for aliasing information for detach and sync instructions.

--- include/llvm/Analysis/AliasAnalysis.h
+++ include/llvm/Analysis/AliasAnalysis.h
@@ -360,6 +360,16 @@ public:
     return getModRefInfo(I, MemoryLocation(P, Size));
   }
 
+  /// getModRefInfo (for detaches) - Return information about whether
+  /// a particular detach modifies or reads the specified memory location.
+  ModRefInfo getModRefInfo(const DetachInst *D, const MemoryLocation &Loc);
+
+  /// getModRefInfo (for detaches) - A convenience wrapper.
+  ModRefInfo getModRefInfo(const DetachInst *D, const Value *P,
+                           uint64_t Size) {
+    return getModRefInfo(D, MemoryLocation(P, Size));
+  }
+
   /// getModRefInfo (for loads) - Return information about whether
   /// a particular load modifies or reads the specified memory location.
   ModRefInfo getModRefInfo(const LoadInst *L, const MemoryLocation &Loc);
@@ -393,11 +403,7 @@ public:
 
   /// getModRefInfo (for syncs) - Return information about whether
   /// a particular store modifies or reads the specified memory location.
-  ModRefInfo getModRefInfo(const SyncInst *S, const MemoryLocation &Loc) {
-    // Conservatively correct.  (We could possibly be a bit smarter if
-    // Loc is a alloca that doesn't escape.)
-    return MRI_ModRef;
-  }
+  ModRefInfo getModRefInfo(const SyncInst *S, const MemoryLocation &Loc);
 
   /// getModRefInfo (for syncs) - A convenience wrapper.
   ModRefInfo getModRefInfo(const SyncInst *S, const Value *P, uint64_t Size) {
@@ -475,6 +481,7 @@ public:
       return getModRefInfo((const AtomicRMWInst*)I, Loc);
     case Instruction::Call:   return getModRefInfo((const CallInst*)I,  Loc);
     case Instruction::Invoke: return getModRefInfo((const InvokeInst*)I,Loc);
+    case Instruction::Detach: return getModRefInfo((const DetachInst*)I,Loc);
     default:
       return MRI_NoModRef;
     }
