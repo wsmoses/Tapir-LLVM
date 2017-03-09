72827d0cc4ef8b3fb556bdb4660c6b0891849b4f
Jiahao Li <isundaylee.reg@gmail.com>
2017-03-09 15:58:45 -0500

    Implements Rosetta-finding part of SyncElimination pass.

--- lib/Transforms/Tapir/SyncElimination.cpp
+++ lib/Transforms/Tapir/SyncElimination.cpp
@@ -8,10 +8,17 @@
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/CFG.h"
+#include "llvm/ADT/SmallSet.h"
+
+#include <deque>
 
 using namespace llvm;
 
 namespace {
+
+typedef SmallSet<const BasicBlock *, 32> BasicBlockSet;
+typedef std::deque<const BasicBlock *> BasicBlockDeque;
 
 struct SyncElimination : public FunctionPass {
   static char ID; // Pass identification, replacement for typeid
@@ -21,16 +28,73 @@ struct SyncElimination : public FunctionPass {
   void getAnalysisUsage(AnalysisUsage &AU) const override {
   }
 
+  // We will explain what Rosetta and Vegas are later. Or rename them.
+  // We promise.
+
+  // Rosetta-finding code
+  // TODO
+
+  // Vegas-finding code
+  //
+  //
+  // We run BFS starting from the sync block, following all foward edges, and stop a branch whenever
+  // we hit another sync block.
+
+  void findVegas(const BasicBlock &BB, BasicBlockSet &OutputSet) {
+    assert(isa<SyncInst>(BB.getTerminator()));
+
+    BasicBlockSet Visited;
+    BasicBlockDeque Frontier;
+
+    Frontier.push_back(&BB);
+
+    while (!Frontier.empty()) {
+      const BasicBlock *Current = Frontier.front();
+      Frontier.pop_front();
+
+      for (const BasicBlock *Succ: successors(Current)) {
+        if (Visited.count(Succ) > 0) {
+          continue;
+        }
+
+        OutputSet.insert(Succ);
+        Visited.insert(Succ);
+
+        // We need to include blocks whose terminator is another sync.
+        // Therefore we still insert the block into OutputSet in this case.
+        // However we do not search any further past the sync block.
+        if (!isa<SyncInst>(Succ->getTerminator())) {
+          Frontier.push_back(Succ);
+        }
+      }
+    }
+  }
+
+  // Entry point code
+
+  void processSyncInstBlock(BasicBlock &BB) {
+    errs() << "SyncElimination: Found sync block: " << BB.getName() << "\n";
+
+    BasicBlockSet RosettaSet, VegasSet;
+
+    findVegas(BB, VegasSet);
+
+    errs() << "SyncElimination:     Blocks found in the Vegas set: " << "\n";
+    for (const BasicBlock *BB: VegasSet) {
+      errs() << "SyncElimination:         " + BB->getName() << "\n";
+    }
+  }
+
   bool runOnFunction(Function &F) override {
     if (skipFunction(F))
       return false;
 
-    errs() << "SyncElimination pass found function: " << F.getName() << "\n";
+    errs() << "SyncElimination: Found function: " << F.getName() << "\n";
     F.setName("sync-elimination_"+F.getName());
 
-    for (BasicBlock &BB: F)
-      if (isa<SyncInst>(BB.getTerminator()))
-      errs() << "Saw basicblock with sync instance " << BB.getName() << "\n";
+    for (BasicBlock &block: F)
+      if (isa<SyncInst>(block.getTerminator()))
+        processSyncInstBlock(block);
 
     return true;
   }
