a57ac4cafdfe845f0c90cc0611705c38f87f1905
Bojan Serafimov <boki@mit.edu>
2017-03-17 16:05:14 -0400

    Add basic SyncElimination tests

---
+++ test/Transforms/Tapir/SyncElimination/basic1.cpp
@@ -0,0 +1,5 @@
+#include <cilk/cilk.h>
+
+void func() {
+  cilk_sync;
+}
---
+++ test/Transforms/Tapir/SyncElimination/main.cpp
@@ -0,0 +1,9 @@
+int a[10];
+int b[10];
+void func() {
+  int x, y;
+  for (int i = 0; i < 10; i++) {
+    a[i] = x + y;
+    b[i] = x + y;
+  }
+}
