cfc3bc5a5bae7dd862334f313044b85839ce38ed
William S. Moses <gh@wsmoses.com>
2018-01-10 14:39:16 -0500

    Add Tapir versioning

--- include/llvm/Config/config.h.cmake
+++ include/llvm/Config/config.h.cmake
@@ -395,8 +395,14 @@
 /* LLVM version string */
 #define LLVM_VERSION_STRING "${PACKAGE_VERSION}"
 
-/* Define if libxml2 is supported on this platform. */
-#cmakedefine LLVM_LIBXML2_ENABLED ${LLVM_LIBXML2_ENABLED}
+/* Major version of the Tapir API */
+#define TAPIR_VERSION_MAJOR ${TAPIR_VERSION_MAJOR}
+
+/* Minor version of the Tapir API */
+#define TAPIR_VERSION_MINOR ${TAPIR_VERSION_MINOR}
+
+/* Patch version of the Tapir API */
+#define TAPIR_VERSION_PATCH ${TAPIR_VERSION_PATCH}
 
 /* Define to the extension used for shared libraries, say, ".so". */
 #cmakedefine LTDL_SHLIB_EXT "${LTDL_SHLIB_EXT}"
