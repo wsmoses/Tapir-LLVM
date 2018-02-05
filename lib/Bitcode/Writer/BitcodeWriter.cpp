12a220590400ae0f696cc144da02f980a8a6d213
TB Schardl <neboat@mit.edu>
2018-02-05 14:42:31 +0000

    [Cilk] Mark functions with continuations that can be stolen in Cilk as "stealable", and ensure that "stealable" functions have opaque modifications to the stack pointer.

--- lib/Bitcode/Writer/BitcodeWriter.cpp
+++ lib/Bitcode/Writer/BitcodeWriter.cpp
@@ -663,6 +663,8 @@ static uint64_t getAttrKindEncoding(Attribute::AttrKind Kind) {
     return bitc::ATTR_KIND_STACK_PROTECT_STRONG;
   case Attribute::SafeStack:
     return bitc::ATTR_KIND_SAFESTACK;
+  case Attribute::Stealable:
+    return bitc::ATTR_KIND_STEALABLE;
   case Attribute::StrictFP:
     return bitc::ATTR_KIND_STRICT_FP;
   case Attribute::StructRet:
