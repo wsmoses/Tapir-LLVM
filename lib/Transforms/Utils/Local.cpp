d3f82db4d1b66683a6a2d6c9b1fca7667e25491e
TB Schardl <neboat@mit.edu>
2018-02-09 14:31:30 +0000

    [Tapir] Updating existing analyses and transformations outside of Tapir lowering to accommodate detach-unwind's and detached_rethrow's.

--- lib/Transforms/Utils/Local.cpp
+++ lib/Transforms/Utils/Local.cpp
@@ -1691,6 +1691,16 @@ void llvm::removeUnwindEdge(BasicBlock *BB) {
   TerminatorInst *TI = BB->getTerminator();
 
   if (auto *II = dyn_cast<InvokeInst>(TI)) {
+    // If we're removing the unwind destination of a detached rethrow, simply
+    // remove the detached rethrow.
+    if (auto *Called = II->getCalledFunction()) {
+      if (Intrinsic::detached_rethrow == Called->getIntrinsicID()) {
+        BranchInst::Create(II->getNormalDest(), II);
+        II->getUnwindDest()->removePredecessor(BB);
+        II->eraseFromParent();
+        return;
+      }
+    }
     changeToCall(II);
     return;
   }
@@ -1710,6 +1720,10 @@ void llvm::removeUnwindEdge(BasicBlock *BB) {
 
     NewTI = NewCatchSwitch;
     UnwindDest = CatchSwitch->getUnwindDest();
+  } else if (auto *DI = dyn_cast<DetachInst>(TI)) {
+    NewTI = DetachInst::Create(DI->getDetached(), DI->getContinue(),
+                               DI->getSyncRegion(), DI);
+    UnwindDest = DI->getUnwindDest();
   } else {
     llvm_unreachable("Could not find unwind successor");
   }
