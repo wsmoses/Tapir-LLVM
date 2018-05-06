28238bfab7288b4d6a0236eef4dd24936eaacb14
TB Schardl <neboat@mit.edu>
2018-05-06 13:13:18 +0000

    [PassManager] Code cleanup for Tapir-lowering passes.

--- lib/Transforms/IPO/PassManagerBuilder.cpp
+++ lib/Transforms/IPO/PassManagerBuilder.cpp
@@ -42,7 +42,6 @@
 #include "llvm/Transforms/Scalar/SimpleLoopUnswitch.h"
 #include "llvm/Transforms/Utils.h"
 #include "llvm/Transforms/Tapir.h"
-#include "llvm/Transforms/Utils/UnifyFunctionExitNodes.h"
 #include "llvm/Transforms/Vectorize.h"
 
 using namespace llvm;
@@ -776,7 +775,6 @@ void PassManagerBuilder::populateModulePassManager(
     // RTS.
 
     MPM.add(createInferFunctionAttrsLegacyPass());
-    // MPM.add(createUnifyFunctionExitNodesPass());
     MPM.add(createLowerTapirToTargetPass(tapirTarget));
     // The lowering pass may leave cruft around.  Clean it up.
     MPM.add(createCFGSimplificationPass());
