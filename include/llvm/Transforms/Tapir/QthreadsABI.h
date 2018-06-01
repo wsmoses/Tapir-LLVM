0c41b9d1bfdc31bfe5961dcd8dc5f9e56d598202
George Stelle <stelleg@lanl.gov>
2018-06-01 16:57:04 -0600

    Partial Qthreads implementation

---
+++ include/llvm/Transforms/Tapir/QthreadsABI.h
@@ -0,0 +1,91 @@
+//===- QthreadsABI.h - Interface to the Qthreads runtime ----*- C++ -*--===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This pass is a simple pass wrapper around the PromoteMemToReg function call
+// exposed by the Utils library.
+//
+//===----------------------------------------------------------------------===//
+#ifndef CILK_ABI_H_
+#define CILK_ABI_H_
+
+#include "llvm/Transforms/Scalar.h"
+#include "llvm/ADT/SmallPtrSet.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/Analysis/AssumptionCache.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Dominators.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/InlineAsm.h"
+#include "llvm/IR/InstIterator.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Intrinsics.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/TypeBuilder.h"
+#include "llvm/IR/ValueSymbolTable.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Transforms/Utils/UnifyFunctionExitNodes.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/Transforms/Utils/Cloning.h"
+#include "llvm/Transforms/Utils/ValueMapper.h"
+#include "llvm/Transforms/Tapir/LoopSpawning.h"
+#include "llvm/Transforms/Tapir/TapirUtils.h"
+#include <deque>
+
+namespace llvm {
+
+/// QthreadsABILoopSpawning uses Qthreads to handle Tapir loops.
+class QthreadsABILoopSpawning : public LoopOutline {
+public:
+  QthreadsABILoopSpawning(Loop *OrigLoop, ScalarEvolution &SE,
+                      LoopInfo *LI, DominatorTree *DT,
+                      AssumptionCache *AC,
+                      OptimizationRemarkEmitter &ORE)
+      : LoopOutline(OrigLoop, SE, LI, DT, AC, ORE)
+  {}
+
+  bool processLoop();
+
+  virtual ~QthreadsABILoopSpawning() {}
+
+protected:
+  // PHINode* canonicalizeIVs(Type *Ty);
+  Value* canonicalizeLoopLatch(PHINode *IV, Value *Limit);
+
+// private:
+//   /// Report an analysis message to assist the user in diagnosing loops that are
+//   /// not transformed.  These are handled as LoopAccessReport rather than
+//   /// VectorizationReport because the << operator of LoopSpawningReport returns
+//   /// LoopAccessReport.
+//   void emitAnalysis(const LoopAccessReport &Message) const {
+//     emitAnalysisDiag(OrigLoop, *ORE, Message);
+//   }
+};
+
+class QthreadsABI : public TapirTarget {
+public:
+  QthreadsABI();
+  virtual ~QthreadsABI(); 
+  Value *GetOrCreateWorker8(Function &F) override final;
+  void createSync(SyncInst &inst, ValueToValueMapTy &DetachCtxToStackFrame)
+    override final;
+
+  Function *createDetach(DetachInst &Detach,
+                         ValueToValueMapTy &DetachCtxToStackFrame,
+                         DominatorTree &DT, AssumptionCache &AC) override final;
+  void preProcessFunction(Function &F) override final;
+  void postProcessFunction(Function &F) override final;
+  void postProcessHelper(Function &F) override final;
+  bool processMain(Function &F) override final;
+};
+
+}  // end of llvm namespace
+
+#endif
