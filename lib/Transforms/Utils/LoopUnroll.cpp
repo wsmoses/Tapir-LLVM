3bdc72a600449e7db787f6a8a59adda63c890c88
TB Schardl <neboat@mit.edu>
2018-10-09 16:24:10 +0000

    [LoopUnroll] Modifying loop unrolling to use and update task info.

--- lib/Transforms/Utils/LoopUnroll.cpp
+++ lib/Transforms/Utils/LoopUnroll.cpp
@@ -23,6 +23,7 @@
 #include "llvm/Analysis/LoopIterator.h"
 #include "llvm/Analysis/OptimizationRemarkEmitter.h"
 #include "llvm/Analysis/ScalarEvolution.h"
+#include "llvm/Analysis/TapirTaskInfo.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/DataLayout.h"
 #include "llvm/IR/DebugInfoMetadata.h"
@@ -37,6 +38,7 @@
 #include "llvm/Transforms/Utils/LoopSimplify.h"
 #include "llvm/Transforms/Utils/LoopUtils.h"
 #include "llvm/Transforms/Utils/SimplifyIndVar.h"
+#include "llvm/Transforms/Utils/TapirUtils.h"
 #include "llvm/Transforms/Utils/UnrollLoop.h"
 using namespace llvm;
 
@@ -165,15 +167,6 @@ foldBlockIntoPredecessor(BasicBlock *BB, LoopInfo *LI, ScalarEvolution *SE,
   BB->eraseFromParent();
 
   return OnlyPred;
-}
-
-//! Identify if a loop could be a cilk for loop and thus diasble unrolling
-bool isCilkFor(Loop* L) {
-  //TODO use a more precise detection of cilk for loops
-  for (BasicBlock* BB : L->blocks())
-    if (dyn_cast<DetachInst>(BB->getTerminator()))
-      return true;
-  return false;
 }
 
 /// Check if unrolling created a situation where we need to insert phi nodes to
@@ -315,7 +308,7 @@ LoopUnrollResult llvm::UnrollLoop(
     bool AllowExpensiveTripCount, bool PreserveCondBr, bool PreserveOnlyFirst,
     unsigned TripMultiple, unsigned PeelCount, bool UnrollRemainder,
     LoopInfo *LI, ScalarEvolution *SE, DominatorTree *DT, AssumptionCache *AC,
-    OptimizationRemarkEmitter *ORE, bool PreserveLCSSA) {
+    TaskInfo *TI, OptimizationRemarkEmitter *ORE, bool PreserveLCSSA) {
 
   BasicBlock *Preheader = L->getLoopPreheader();
   if (!Preheader) {
@@ -388,8 +381,12 @@ LoopUnrollResult llvm::UnrollLoop(
 
   // Are we eliminating the loop control altogether?
   bool CompletelyUnroll = Count == TripCount;
-  if (isCilkFor(L) && !CompletelyUnroll)
+
+  // Disallow partial unrolling of Tapir loops.
+  // FIXME: Allow partial unrolling of a Tapir loop to produce a new Tapir loop.
+  if (getTaskIfTapirLoop(L, TI) && !CompletelyUnroll)
     return LoopUnrollResult::Unmodified;
+
   SmallVector<BasicBlock *, 4> ExitBlocks;
   L->getExitBlocks(ExitBlocks);
   std::vector<BasicBlock*> OriginalLoopBlocks = L->getBlocks();
@@ -887,6 +884,12 @@ LoopUnrollResult llvm::UnrollLoop(
       for (Loop *SubLoop : LoopsToSimplify)
         simplifyLoop(SubLoop, DT, LI, SE, AC, PreserveLCSSA);
     }
+
+    // Update TaskInfo manually using the updated DT.
+    if (TI)
+      // FIXME: Recalculating TaskInfo for the whole function is wasteful.
+      // Optimize this routine in the future.
+      TI->recalculate(*Header->getParent(), *DT);
   }
 
   return CompletelyUnroll ? LoopUnrollResult::FullyUnrolled
