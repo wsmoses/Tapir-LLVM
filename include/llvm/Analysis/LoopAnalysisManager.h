602378c2e209171de9f8eb97de897d034a4db108
TB Schardl <neboat@mit.edu>
2018-10-09 16:02:02 +0000

    [LoopAnalysis] Add TapirTaskInfo analysis to loop passes managed by the new pass manager.

--- include/llvm/Analysis/LoopAnalysisManager.h
+++ include/llvm/Analysis/LoopAnalysisManager.h
@@ -40,6 +40,7 @@
 #include "llvm/Analysis/MemorySSA.h"
 #include "llvm/Analysis/ScalarEvolution.h"
 #include "llvm/Analysis/ScalarEvolutionAliasAnalysis.h"
+#include "llvm/Analysis/TapirTaskInfo.h"
 #include "llvm/Analysis/TargetLibraryInfo.h"
 #include "llvm/Analysis/TargetTransformInfo.h"
 #include "llvm/IR/Dominators.h"
@@ -59,6 +60,7 @@ struct LoopStandardAnalysisResults {
   ScalarEvolution &SE;
   TargetLibraryInfo &TLI;
   TargetTransformInfo &TTI;
+  TaskInfo &TI;
   MemorySSA *MSSA;
 };
 
