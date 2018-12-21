6572fa1d94da679941246bdf239e1486039cf768
TB Schardl <neboat@mit.edu>
2018-12-21 05:07:53 +0000

    [CSI] Add support for running the CSI instrumentation pass at a specified extension point during the optimization pipeline.

--- lib/Transforms/IPO/PassManagerBuilder.cpp
+++ lib/Transforms/IPO/PassManagerBuilder.cpp
@@ -452,6 +452,7 @@ void PassManagerBuilder::populateModulePassManager(
 
     // Add passes to run just before Tapir lowering.
     addExtensionsToPM(EP_TapirLate, MPM);
+    addExtensionsToPM(EP_TapirLoopEnd, MPM);
 
     if (TapirTargetID::None != TapirTarget) {
       // MPM.add(createAnalyzeTapirPass());
@@ -807,6 +808,7 @@ void PassManagerBuilder::populateModulePassManager(
       MPM.add(createMergeFunctionsPass());
     MPM.add(createBarrierNoopPass());
     // addFunctionSimplificationPasses(MPM);
+    addExtensionsToPM(EP_TapirLoopEnd, MPM);
 
     // Now lower Tapir to Target runtime calls.
     //
