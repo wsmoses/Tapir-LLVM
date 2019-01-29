6034812a2e60129163b60121c93ab027646f09c8
Daniele Vettorel <vettoreldaniele@gmail.com>
2019-01-29 17:03:51 -0500

    Fixes to how runtime spawns are tracked. Fixes to function interpositioning

--- include/llvm/Transforms/Instrumentation.h
+++ include/llvm/Transforms/Instrumentation.h
@@ -217,6 +217,7 @@ struct CSIOptions {
   bool InstrumentTapir = true;
   bool InstrumentAllocas = true;
   bool InstrumentAllocFns = true;
+  bool Interpose = true;
 
   CSIOptions() = default;
 };
