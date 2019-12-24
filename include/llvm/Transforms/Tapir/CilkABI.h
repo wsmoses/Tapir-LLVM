c791efa2cc9043695de1a665397f5ed6cc318a37
TB Schardl <neboat@mit.edu>
2019-12-24 15:09:58 +0000

    [TapirToTarget] [LoopSpawningTI] Change Tapir lowering to support different approaches for lowering to the Cilk Plus runtime system.  In particular:
    [TapirToTarget] Add support for using argument structures for outlined
     Tapir tasks and Tapir loops.
    
    [LoopSpawningTI] Add support for lowering Tapir loops to calls to
    __cilkrts_cilk_for runtime methods.
    
    Although these approaches for lowering to the Cilk Plus runtime system
    likely won't be used in practice, the functionality in the
    Tapir-lowering infrastructure to support these capabilities should be
    useful for other Tapir targets.

--- include/llvm/Transforms/Tapir/CilkABI.h
+++ include/llvm/Transforms/Tapir/CilkABI.h
@@ -16,6 +16,7 @@
 
 #include "llvm/Transforms/Tapir/LoopSpawning.h"
 #include "llvm/Transforms/Tapir/LoweringUtils.h"
+#include "llvm/Transforms/Tapir/TapirLoopInfo.h"
 
 namespace llvm {
 class Value;
@@ -48,7 +49,10 @@ public:
   Value *lowerGrainsizeCall(CallInst *GrainsizeCall) override final;
   void lowerSync(SyncInst &inst) override final;
 
-  void preProcessFunction(Function &F) override final;
+  ArgStructMode getArgStructMode() const override final;
+  void addHelperAttributes(Function &F) override final;
+
+  void preProcessFunction(Function &F, TaskInfo &TI) override final;
   void postProcessFunction(Function &F) override final;
   void postProcessHelper(Function &F) override final;
 
@@ -57,10 +61,40 @@ public:
   void processSubTaskCall(TaskOutlineInfo &TOI, DominatorTree &DT)
     override final;
 
+  LoopOutlineProcessor *getLoopOutlineProcessor(const TapirLoopInfo *TL) const
+    override final;
+
   struct __cilkrts_pedigree {};
   struct __cilkrts_stack_frame {};
   struct __cilkrts_worker {};
+};
 
+/// The RuntimeCilkFor loop-outline processor transforms an outlined Tapir loop
+/// to be processed using a call to a runtime method __cilkrts_cilk_for_32 or
+/// __cilkrts_cilk_for_64.
+class RuntimeCilkFor : public LoopOutlineProcessor {
+  Type *GrainsizeType = nullptr;
+public:
+  RuntimeCilkFor(Module &M) : LoopOutlineProcessor(M) {
+    GrainsizeType = Type::getInt32Ty(M.getContext());
+  }
+
+  ArgStructMode getArgStructMode() const override final {
+    // return ArgStructMode::Dynamic;
+    return ArgStructMode::Static;
+  }
+  void setupLoopOutlineArgs(
+      Function &F, ValueSet &HelperArgs, SmallVectorImpl<Value *> &HelperInputs,
+      ValueSet &InputSet, const SmallVectorImpl<Value *> &LCArgs,
+      const SmallVectorImpl<Value *> &LCInputs,
+      const ValueSet &TLInputsFixed)
+    override final;
+  unsigned getIVArgIndex(const Function &F, const ValueSet &Args) const
+    override final;
+  void postProcessOutline(TapirLoopInfo &TL, TaskOutlineInfo &Out,
+                          ValueToValueMapTy &VMap) override final;
+  void processOutlinedLoopCall(TapirLoopInfo &TL, TaskOutlineInfo &TOI,
+                               DominatorTree &DT) override final;
 };
 }  // end of llvm namespace
 
